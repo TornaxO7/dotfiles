3fdbe013b249196c4cc3239986e5e59aca40f714
TornaxO7 <tornax@pm.me>
2023-06-07 20:35:30 +0200

    adding home manager

--- flake.nix
+++ flake.nix
@@ -3,10 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    # home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager";
   };
 
-  outputs = inputs@{ self, nixpkgs }:
+  outputs = inputs@{ self, nixpkgs, home-manager }:
   let
     forEachSystem = init_function: nixpkgs.lib.genAttrs [
       "x86_64-linux"
