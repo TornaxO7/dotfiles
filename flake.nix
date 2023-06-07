0e973d7a1184447ee5758a82021d2b3a76a3cdd0
TornaxO7 <tornax@pm.me>
2023-06-07 10:21:33 +0200

    yes

--- flake.nix
+++ flake.nix
@@ -1,6 +1,20 @@
 {
     description = "Flake dotfiles from TornaxO7";
 
-    output = {self, nixpkgs}: {
-    }
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+        # home-manager.url = "github:nix-community/home-manager";
+    };
+
+    outputs = inputs@{self, nixpkgs, flake-utils}: {
+        let
+            username = "tornax";
+            init_system = {system ? "x86_64-linux"}: {
+            :
+            };
+        in {
+            nixosConfigurations."TORNAX-MAIN" = init_system {
+            };
+        };
+    };
 }
