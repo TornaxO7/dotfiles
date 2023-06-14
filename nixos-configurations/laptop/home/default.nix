14d1f16dae209cb1600826893b3d6efc75aa4afe
TornaxO7 <tornax@pm.me>
2023-06-14 14:23:46 +0200

    adding i3status-rust to laptop

--- nixos-configurations/laptop/home/default.nix
+++ nixos-configurations/laptop/home/default.nix
@@ -4,4 +4,5 @@
     packages = import ./packages.nix { inherit pkgs; };
   };
   programs = import ./programs.nix;
+  xdg = import ./xdg.nix;
 }
---
+++ nixos-configurations/laptop/home/xdg.nix
@@ -0,0 +1,10 @@
+{
+  configFile = {
+    i3status-rust = {
+      enabled = true;
+      recursive = true;
+      source = ../config/i3status-rust;
+      target = "i3status-rust";
+    };
+  };
+}
