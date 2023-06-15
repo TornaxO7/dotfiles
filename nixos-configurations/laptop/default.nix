a9be23b4c2f8e1e389b575a4bf90caf8f38a789e
TornaxO7 <tornax@pm.me>
2023-06-15 11:39:13 +0200

    laptop: fixing environment variables scales

--- nixos-configurations/laptop/default.nix
+++ nixos-configurations/laptop/default.nix
@@ -6,9 +6,9 @@
   ];
 
   environment.variables = {
-    GDK_SCALE = 2;
-    GDK_DPI_SCALE = 0.5;
-    QT_AUTO_SCREEN_SCALE_FACTOR = 2;
+    GDK_SCALE = "2";
+    GDK_DPI_SCALE = "0.5";
+    QT_AUTO_SCREEN_SCALE_FACTOR = "2";
   };
 
   imports = [
