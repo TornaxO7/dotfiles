d34e12c13b31b393a107f3d302a14eba36245af7
TornaxO7 <tornax@proton.me>
2023-06-23 21:32:18 +0200

    fixing pointerCursor size on pc

--- nixos-configurations/pc/home/default.nix
+++ nixos-configurations/pc/home/default.nix
@@ -4,7 +4,10 @@
     ./i3.nix
   ];
 
-  home.packages = import ./packages.nix { inherit pkgs; };
+  home = {
+    packages = import ./packages.nix { inherit pkgs; };
+    pointerCursor.size = 20;
+  };
   services = import ./services.nix;
   xdg = import ./xdg.nix;
 }
