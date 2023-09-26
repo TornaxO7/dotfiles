5e6919ce68f9be78d3d5799410c7f700743a5e05
TornaxO7 <tornax@proton.me>
2023-09-26 17:25:04 +0200

    shells: fix rust shell

--- shells/rust/default.nix
+++ shells/rust/default.nix
@@ -2,13 +2,24 @@ let
   rust-overlay = builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz";
 
   pkgs = import <nixpkgs> {
-    overlays = [(import rust-overlay)];
+    overlays = [ (import rust-overlay) ];
   };
 
   toolchain = pkgs.rust-bin.fromRustupToolchainFile ./toolchain.toml;
 in
-  pkgs.mkShell {
-    packages = [
-      toolchain
-    ];
-  }
+pkgs.mkShell rec {
+  packages = [ toolchain ];
+
+  buildInputs = with pkgs; [
+    rustup
+
+    xorg.libX11
+    xorg.libXcursor
+    xorg.libXrandr
+    xorg.libXi
+  ];
+
+  shellHook = ''
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${builtins.toString (pkgs.lib.makeLibraryPath buildInputs)}";
+  '';
+}
