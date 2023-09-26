25b78909478c7f5a94cb1f762c713e3874cf7b56
TornaxO7 <tornax@proton.me>
2023-09-27 00:51:13 +0200

    shells, rust: adding vulkan packages

--- shells/rust/default.nix
+++ shells/rust/default.nix
@@ -13,10 +13,11 @@ pkgs.mkShell rec {
   buildInputs = with pkgs; [
     rustup
 
-    xorg.libX11
-    xorg.libXcursor
-    xorg.libXrandr
-    xorg.libXi
+    xorg.libX11 xorg.libXcursor xorg.libXrandr xorg.libXi
+
+    shaderc directx-shader-compiler libGL
+    vulkan-headers vulkan-loader vulkan-tools vulkan-tools-lunarg
+    vulkan-validation-layers
   ];
 
   shellHook = ''
