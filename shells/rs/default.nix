4ac309507d84e7be4fa1f096cfc28bc417d4384c
TornaxO7 <tornax@proton.me>
2023-10-06 11:54:27 +0200

    updating rust shell

--- shells/rs/default.nix
+++ shells/rs/default.nix
@@ -13,12 +13,24 @@ pkgs.mkShell rec {
   buildInputs = with pkgs; [
     rustup
 
-    xorg.libX11 xorg.libXcursor xorg.libXrandr xorg.libXi
+    xorg.libX11
+    xorg.libXcursor
+    xorg.libXrandr
+    xorg.libXi
+    xorg.libxkbfile
+    xorg.xkbutils
+    xorg.xkbevd
+    libxkbcommon
 
     pkg-config
 
-    shaderc directx-shader-compiler libGL
-    vulkan-headers vulkan-loader vulkan-tools vulkan-tools-lunarg
+    shaderc
+    directx-shader-compiler
+    libGL
+    vulkan-headers
+    vulkan-loader
+    vulkan-tools
+    vulkan-tools-lunarg
     vulkan-validation-layers
 
     cmake
