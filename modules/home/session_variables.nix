c245173be06dfb39dfd857420360a9c7c69ebdbc
TornaxO7 <tornax@proton.me>
2023-10-28 20:13:44 +0200

    updating NIX_PATH

--- modules/home/session_variables.nix
+++ modules/home/session_variables.nix
@@ -3,6 +3,6 @@
   SUDO_EDITOR = "nvim";
   MANPAGER = "nvim +Man!";
   PATH = "$PATH:~/.local/bin";
-  NIX_PATH = "$NIX_PATH:/home/tornax/dotfiles/shells";
+  NIX_PATH = "~/.nix-defexpr/channels:$NIX_PATH:/home/tornax/dotfiles/shells";
   MOZ_USE_XINPUT2 = "1";
 }
