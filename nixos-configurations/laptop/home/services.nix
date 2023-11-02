fbc52bf01412d922aeb60fda2a8b44336c241046
TornaxO7 <tornax@proton.me>
2023-11-02 10:39:53 +0100

    laptop: enabling i3locker

--- nixos-configurations/laptop/home/services.nix
+++ nixos-configurations/laptop/home/services.nix
@@ -1,6 +1,9 @@
 { config, pkgs, lib, ... }:
 {
   config = {
-    services.blueman-applet.enable = true;
+    services = {
+      blueman-applet.enable = true;
+      screen-locker.enable = true;
+    };
   };
 }
