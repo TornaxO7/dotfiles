8a0313973cf70cd9641fa3bc62ad307c1207256a
TornaxO7 <tornax@proton.me>
2023-11-04 01:19:18 +0100

    shell_aliases: removing `eplug`

--- modules/home/shell_aliases.nix
+++ modules/home/shell_aliases.nix
@@ -21,7 +21,6 @@
   ef = "fzf | read yeet; if [[ \$yeet != '' ]]; then $EDITOR \$yeet; fi";
   eja = "$EDITOR /tmp/Test.java";
   ea = "$EDITOR /tmp/test.s";
-  # eplug = "$EDITOR ~/.config/nvim/init.vim";
   en = "$EDITOR /tmp/test.norg";
 
   sp = "sudo poweroff";
