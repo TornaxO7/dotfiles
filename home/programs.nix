f3a4eb160b15fd6542c91b09c3fbda714a45276c
TornaxO7 <tornax@proton.me>
2023-11-30 00:11:45 +0100

    using `gitoxide` for git

--- home/programs.nix
+++ home/programs.nix
@@ -164,6 +164,7 @@
 
     git = {
       enable = true;
+      package = pkgs.gitoxide;
       delta.enable = true;
       signing = {
         key = "7559 3129 41F8 AAAD 9EB6  D913 F652 0002 D62D 6194";
