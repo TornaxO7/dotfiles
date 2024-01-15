24cf63e865087712ca398e82596dcb2824308e74
TornaxO7 <tornax@proton.me>
2024-01-15 16:41:28 +0100

    haskell: adding `ghc-vis`

--- shell/haskell/default.nix
+++ shell/haskell/default.nix
@@ -4,6 +4,7 @@ let
 
 
   myDevTools = with hpkgs; [
+    ghc-vis
     ghc # GHC compiler in the desired version (will be available on PATH)
     ghcid # Continuous terminal Haskell compile checker
     ormolu # Haskell formatter
