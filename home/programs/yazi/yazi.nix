7db7d2fadc15a85f3e302478ea13a0b7884b7f42
TornaxO7 <tornax@proton.me>
2024-01-20 14:07:15 +0100

    yazi: fix default sort

--- home/programs/yazi/yazi.nix
+++ home/programs/yazi/yazi.nix
@@ -4,7 +4,7 @@
     ratio = [ 1 4 3 ];
     sort_by = "alphabetical";
     sort_sensitive = true;
-    sort_reverse = true;
+    sort_reverse = false;
     sort_dir_first = true;
     linemode = "size";
     show_hidden = false;
