63ea2502ebc1bc38ab8238f1a9fa8f475b5de7fb
TornaxO7 <tornax@proton.me>
2024-01-23 21:36:30 +0100

    removing pc deployment

--- modules/deploy.nix
+++ modules/deploy.nix
@@ -3,14 +3,6 @@
   flake = {
     deploy = {
       nodes = {
-        pc = {
-          hostname = "localhost";
-          profiles.system = {
-            user = "root";
-            path = inputs.deploy-rs.lib.${self.nixosConfigurations.laptop.pkgs.system}.activate.nixos self.nixosConfigurations.pc;
-          };
-        };
-
         laptop = {
           hostname = "laptop";
           profiles.system = {
