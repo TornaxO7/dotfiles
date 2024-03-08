cb28f56e7423eb4e027eb56763ac2c4d68f164df
TornaxO7 <tornax@proton.me>
2024-03-08 13:48:53 +0100

    fix env variable for QT scale

--- systems/laptop/default.nix
+++ systems/laptop/default.nix
@@ -11,7 +11,7 @@
       variables = {
         GDK_SCALE = "2";
         GDK_DPI_SCALE = "0.5";
-        QT_AUTO_SCREEN_SCALE_FACTOR = "2";
+        QT_AUTO_SCREEN_SCALE_FACTOR = "True";
       };
     };
 
