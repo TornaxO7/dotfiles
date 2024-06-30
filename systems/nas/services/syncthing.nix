7fe6d7153b43fcd3031cb1fd2cae6cf1ca154eb4
TornaxO7 <tornax@proton.me>
2024-06-30 13:25:20 +0200

    nas: syncthing disable relay per default

--- systems/nas/services/syncthing.nix
+++ systems/nas/services/syncthing.nix
@@ -8,6 +8,7 @@
     services.syncthing = {
       enable = true;
       user = username;
+      relay.enable = false;
       openDefaultPorts = true;
       guiAddress = "100.88.51.57:8040";
     };
