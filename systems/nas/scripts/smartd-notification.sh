09fdb7156f2f251cfdf6e70275924ba92289a1a9
TornaxO7 <tornax@proton.me>
2024-08-22 00:20:25 +0200

    nas: trying to fix zpool status

--- systems/nas/scripts/smartd-notification.sh
+++ systems/nas/scripts/smartd-notification.sh
@@ -2,4 +2,5 @@
 #! nix-shell -i bash -p bash curl coreutils
 
 HOOK="$(cat /run/agenix/discord-webhook)"
-echo 'curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "{\"content\": \"Found smart-error! Please take a look on it.\"}" "$HOOK"'
+# echo 'curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "{\"content\": \"Found smart-error! Please take a look on it.\"}" "$HOOK"'
+curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "{\"content\": \"Found smart-error! Please take a look on it.\"}" "$HOOK"
--- systems/nas/zfs/default.nix
+++ systems/nas/zfs/default.nix
@@ -1,8 +1,19 @@
 { config, pkgs, ... }:
 let
-  ip-address = "100.88.51.57";
   zpool-name = "hdds";
-  discord-notify = msg: ''${pkgs.curl}/bin/curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "{\"content\": \"'' + msg + ''\"}" $(${pkgs.coreutils}/bin/cat ${config.age.secrets.discord-webhook.path})'';
+
+  zfsCheckScript = ''
+    POOL_STATUS=$(${pkgs.zfs}/bin/zpool status -x ${zpool-name})
+    DISCORD_HOOK=$(${pkgs.coreutils}/bin/cat ${config.age.secrets.discord-webhook.path})
+
+    if [[ $POOL_STATUS == *"errors" ]]; then
+      ${pkgs.curl}/bin/curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "{\"content\": \"zpool contains errors\"}" $DISCORD_HOOK
+    else
+      ${pkgs.curl}/bin/curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "{\"content\": \"zpool is healthy\"}" $DISCORD_HOOK
+    fi
+  '';
+
+  zfsCheckScriptPath = pkgs.writeScriptBin "zfs-check-script" zfsCheckScript;
 in
 {
   config = {
@@ -14,138 +25,24 @@ in
 
     systemd = {
       services."zpool-check" = {
-        script = ''
-          STATUS=$(${pkgs.zfs}/bin/zpool get -H health ${zpool-name} | ${pkgs.gawk}/bin/awk '{print $3}')
-
-          if [[ "$STATUS" != "ONLINE" ]]; then
-            ${discord-notify "zpool has a problem!"}
-          else
-            ${discord-notify "zpool is clean."}
-          fi
-        '';
-
-        wants = [ "zfs-scrub.service" ];
-        after = [ "zfs-scrub.service" ];
-
+        description = "ZFS Pool check";
         serviceConfig = {
+          ExecStart = "${pkgs.bash}/bin/bash ${zfsCheckScriptPath}/bin/zfs-check-script";
           Type = "oneshot";
-          User = "root";
+        };
+      };
+
+      timers.check-zfs-timer = {
+        description = "RUn ZFS status check daily";
+        wants = [ "zpool-check.service" ];
+        timerConfig = {
+          OnCalendar = "daily";
+          Persistent = true;
         };
       };
     };
 
     services = {
-      grafana = {
-        enable = false;
-        settings = {
-          server = {
-            http_addr = ip-address;
-            http_port = 3000;
-          };
-
-          log = {
-            mode = "file";
-            level = "error";
-          };
-        };
-
-        provision = {
-          # dashboards.settings.providers = [{
-          #   name = "default";
-          #   options.path = "/var/lib/grafana/dashboards";
-          # }];
-
-          datasources.settings.datasources = [{
-            name = "Prometheus";
-            type = "prometheus";
-            access = "proxy";
-            url = "http://localhost:9090/";
-          }];
-        };
-      };
-
-      prometheus = {
-        enable = false;
-
-        globalConfig = {
-          scrape_interval = "15s";
-          evaluation_interval = "15s";
-        };
-
-        alertmanagers = [{
-          static_configs = [{
-            targets = [ "localhost:9093" ];
-          }];
-        }];
-
-        ruleFiles = [
-          ./zfs-pool-degraded-alert.yml
-          ./test_alert.yml
-        ];
-
-        alertmanager =
-          let
-            receiver = "discord";
-          in
-          {
-            enable = true;
-            logFormat = "logfmt";
-            configuration = {
-              route = {
-                group_by = [ "alertname" "job" ];
-
-                group_wait = "30s";
-                group_interval = "5m";
-                repeat_interval = "3h";
-
-                inherit receiver;
-              };
-
-              receivers = [{
-                name = receiver;
-                discord_configs = [{
-                  send_resolved = true;
-                  webhook_url_file = config.age.secrets.discord-webhook.path;
-                }];
-              }];
-            };
-          };
-
-        scrapeConfigs = [
-          {
-            job_name = "node";
-            static_configs = [{
-              targets = [
-                " localhost:${toString config.services.prometheus.exporters.node.port}"
-              ];
-            }];
-          }
-          {
-            job_name = "zfs";
-            static_configs = [{
-              targets = [
-                "localhost:${toString config.services.prometheus.exporters.zfs.port}"
-              ];
-            }];
-          }
-          {
-            job_name = "demo";
-            static_configs = [{
-              targets = [
-                "demo.promlabs.com:10000"
-                "demo.promlabs.com:10001"
-                "demo.promlabs.com:10002"
-              ];
-            }];
-          }
-        ];
-
-        exporters = {
-          node.enable = true;
-          zfs.enable = true;
-        };
-      };
-
       zfs = {
         autoScrub = {
           enable = true;
@@ -156,13 +53,6 @@ in
           enable = true;
           interval = "quarterly";
         };
-      };
-
-      smartd = {
-        enable = true;
-        extraOptions = [
-          "-w '${discord-notify "smart-test failed!"}'"
-        ];
       };
     };
   };
