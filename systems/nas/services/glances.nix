075e97a12d26ce661f6d9bdbc4e18a3ffce2182c
TornaxO7 <tornax@proton.me>
2024-08-23 15:27:07 +0200

    nas: fix glances api issue

--- systems/nas/services/glances.nix
+++ systems/nas/services/glances.nix
@@ -1,11 +1,11 @@
-{ pkgs, ... }:
+{ unstable, ... }:
 {
   config = {
     systemd.services."glances" = {
       description = "Glances service https://github.com/nicolargo/glances";
       wants = [ "podman.socket" ];
       serviceConfig = {
-        ExecStart = "${pkgs.glances}/bin/glances -w --disable-webui -p 49200";
+        ExecStart = "${unstable.glances}/bin/glances -w --disable-webui -p 49200";
       };
       environment = {
         TZ = "Europe/Berlin";
