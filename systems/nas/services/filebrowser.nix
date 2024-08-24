0b30a8aa81b78632712a7b526425578d399a330f
TornaxO7 <tornax@proton.me>
2024-08-24 13:21:40 +0200

    nas: fix filebrowser database issue

--- systems/nas/services/filebrowser.nix
+++ systems/nas/services/filebrowser.nix
@@ -1,21 +1,20 @@
 port: { username, zpool-root, ... }:
 let
-  utils = import ../utils.nix;
   portStr = toString port;
 
   filebrowser-root = "${zpool-root}/filebrowser";
-  database-path = "${filebrowser-root}/filebrowser.db";
+  database-path = "${filebrowser-root}/database.db";
 in
 {
   config = {
-    systemd.tmpfiles.settings.filebrowser = utils.createDirs username [ database-path ];
+    systemd.tmpfiles.settings.filebrowser.${database-path}.f.user = username;
 
     virtualisation.oci-containers.containers.filebrowser = {
       image = "filebrowser/filebrowser";
       ports = [ "${portStr}:80" ];
       volumes = [
         "${zpool-root}/syncthing:/srv"
-        "${database-path}:/database/filebrowser.db"
+        "${database-path}:/database.db"
       ];
     };
   };
