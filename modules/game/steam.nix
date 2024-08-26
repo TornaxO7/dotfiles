8ddf642709c0d1405ccf5f3af7bfcce98dc73260
TornaxO7 <tornax@proton.me>
2024-08-26 19:38:28 +0200

    pc: steam enabling remote play

--- modules/game/steam.nix
+++ modules/game/steam.nix
@@ -4,6 +4,8 @@
     programs.steam = {
       enable = true;
       remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = true;
+      localNetworkGameTransfers.openFirewall = true;
     };
 
     services.xserver.desktopManager.plasma5.enable = true;
