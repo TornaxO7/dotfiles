63e554d0a0e38ccba9b2437edf6d3232d4630f15
TornaxO7 <tornax@proton.me>
2024-09-22 22:26:51 +0200

    nas: adding auto docker-image puller for updates

--- systems/nas/default.nix
+++ systems/nas/default.nix
@@ -1,6 +1,20 @@
 { pkgs, ... }:
 let
   loadService = path: port: (import path) port;
+
+  update-containers = pkgs.writeShellScriptBin "update-containers" ''
+    	SUDO=""
+    	if [[ $(id -u) -ne 0 ]]; then
+    		SUDO="sudo"
+    	fi
+
+        images=$($SUDO ${pkgs.podman}/bin/podman ps -a --format="{{.Image}}" | sort -u)
+
+        for image in $images
+        do
+          $SUDO ${pkgs.podman}/bin/podman pull $image
+        done
+  '';
 in
 {
   imports = [
@@ -47,6 +61,25 @@ in
       };
 
       oci-containers.backend = "podman";
+    };
+
+    systemd = {
+      updatecontainers = {
+        timerConfig = {
+          Unit = "updatecontainers.service";
+          OnCalendar = "Mon 02:00";
+        };
+        wantedBy = [ "timers.target" ];
+      };
+
+      services = {
+        updatecontainers = {
+          serviceConfig = {
+            Type = "oneshot";
+            ExecStart = "${update-containers}";
+          };
+        };
+      };
     };
   };
 }
