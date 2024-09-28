c2c2f5da9f700f0c038ad589428bc6a1abb403f7
TornaxO7 <tornax@proton.me>
2024-09-28 15:11:49 +0200

    nas: adding more domains for homarr because somehow dashboard.local doesn't works trustworthy

--- systems/nas/services/homarr.nix
+++ systems/nas/services/homarr.nix
@@ -13,7 +13,7 @@
 
     labels = {
       "traefik.enable" = "true";
-      "traefik.http.routers.homarr.rule" = "Host(`dashboard.local`)";
+      "traefik.http.routers.homarr.rule" = "Host(`dashboard.local`) || Host(`dash.local`) || Host(`main.local`)";
       "traefik.http.routers.homarr.service" = "homarr";
       "traefik.http.services.homarr.loadbalancer.server.port" = toString 7575;
     };
