3e86064397ed6b754e6518697b19789facdf49d3
TornaxO7 <tornax@proton.me>
2024-10-21 09:03:17 +0200

    removing trusted substituters

--- modules/default_main.nix
+++ modules/default_main.nix
@@ -6,11 +6,11 @@
   ];
 
   config = {
-    nix.settings = {
-      substituters = [ "http://nas:49310" ];
-      trusted-public-keys = [ "cache.nas:nlGlXrh+kDHcfuzhyiSVkUVNKA6snAaCU77R7dXCXAY=" ];
-      connect-timeout = 3;
-    };
+    # nix.settings = {
+    #   substituters = [ "http://nas:49310" ];
+    #   trusted-public-keys = [ "cache.nas:nlGlXrh+kDHcfuzhyiSVkUVNKA6snAaCU77R7dXCXAY=" ];
+    #   connect-timeout = 3;
+    # };
 
     environment.systemPackages = with pkgs; [
       tailscale
