dd4f2597158f6b9056ab8e2d40bdf959a9d4b28b
TornaxO7 <tornax@proton.me>
2024-11-25 23:19:55 +0100

    server: disabling headscale

--- systems/server/services/headscale.nix
+++ systems/server/services/headscale.nix
@@ -5,16 +5,19 @@ let
   paths = rec {
     root = "${services-root}/headscale";
     config = "${root}/headscale-config";
+    data = "${root}/headscale-data";
   };
 
-  names = utils.createContainerNames "headscale" [ "server" ];
-  domain = "headscale.${domain-root}";
+  names = utils.createContainerNames "headscale" [ "server" "webui" ];
+  server-domain = "headscale.${domain-root}";
+  ui-domain = "ui.${server-domain}";
 in
 {
   systemd = {
     tmpfiles.settings = {
-      "${paths.root}" = utils.createDirs config [ paths.root ];
-      "${paths.config}" = utils.createDirs config [ paths.config ];
+      headscale-root = utils.createDirs config [ paths.root ];
+      headscale-config = utils.createDirs config [ paths.config ];
+      headscale-data = utils.createDirs config [ paths.data ];
     };
 
     services = {
@@ -24,24 +27,38 @@ in
 
   virtualisation.oci-containers.containers = {
     "${names.containers.server}" = {
-      image = "headscale/headscale:latest";
+      image = "headscale/headscale:stable";
       volumes = [
         "${paths.config}:/etc/headscale"
+        "${paths.data}:/var/lib/headscale"
       ];
       labels = {
         "traefik.enable" = "true";
 
-        "traefik.http.routers.headscale-8080.rule" = "Host(`${domain}`)";
+        "traefik.http.routers.headscale-8080.rule" = "Host(`${server-domain}`)";
         "traefik.http.routers.headscale-8080.service" = "headscale-8080";
         "traefik.http.routers.headscale-8080.tls" = "true";
         "traefik.http.routers.headscale-8080.tls.certresolver" = "main";
         "traefik.http.services.headscale-8080.loadbalancer.server.port" = "8080";
 
-        "traefik.http.routers.headscale-9090.rule" = "Host(`${domain}`)";
+        "traefik.http.routers.headscale-9090.rule" = "Host(`${server-domain}`)";
         "traefik.http.routers.headscale-9090.service" = "headscale-9090";
         "traefik.http.routers.headscale-9090.tls" = "true";
         "traefik.http.routers.headscale-9090.tls.certresolver" = "main";
         "traefik.http.services.headscale-9090.loadbalancer.server.port" = "9090";
+      };
+    };
+
+    "${names.containers.webui}" = {
+      image = "ghcr.io/gurucomputing/headscale-ui:latest";
+
+      labels = {
+        "traefik.enable" = "true";
+        "traefik.http.routers.hoadscale-ui.rule" = "Host(`${ui-domain}`)";
+        "traefik.http.routers.hoadscale-ui.service" = names.containers.webui;
+        "traefik.http.routers.hoadscale-ui.tls" = "true";
+        "traefik.http.routers.hoadscale-ui.tls.certresolver" = "main";
+        "traefik.http.services.${names.containers.webui}.loadbalancer.server.port" = "8443";
       };
     };
   };
