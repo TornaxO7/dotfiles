c69123715bbce4b8387b66cf20414187e20b231c
TornaxO7 <tornax@proton.me>
2024-11-26 21:07:26 +0100

    nas + server: trying to provide token for watchtower and prepare headscale

--- systems/nas/services/watchtower.nix
+++ systems/nas/services/watchtower.nix
@@ -1,4 +1,4 @@
-utils: { ... }:
+utils: { config, ... }:
 {
   virtualisation.oci-containers.containers.watchtower = {
     image = "containrrr/watchtower";
@@ -9,6 +9,11 @@ utils: { ... }:
 
     environment = {
       TZ = "DE";
+      WATCHTOWER_NOTIFICATIONS = "gotify";
+      WATCHTOWER_NOTIFICATION_GOTIFY_TLS_SKIP_VERIFY = "true";
+      WATCHTOWER_NOTIFICATION_GOTIFY_URL = "http://gotify.nas.local";
+      WATCHTOWER_NOTIFICATION_GOTIFY_TOKEN = config.age.secrets.gotify-token.path;
     };
   };
 }
+
