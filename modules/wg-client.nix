f2c9ff5c290badc5fde8047c9cd7b4f829530ea9
TornaxO7 <tornax@proton.me>
2025-01-03 03:24:09 +0100

    wg-server add name

--- modules/wg-client.nix
+++ modules/wg-client.nix
@@ -13,6 +13,7 @@
 
           peers = [
             {
+              name = "server";
               publicKey = "Sp+GoX7YlHcdxXFX40GvWKWA/Hm6FFfmzI5CgNvsxBk=";
               allowedIPs = [ "10.0.0.0/24" ];
               endpoint = "2.56.97.207:51820";
