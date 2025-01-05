f4d739a9aed76a1e36ab34cd40ace60ab70fb1ca
TornaxO7 <tornax@proton.me>
2025-01-05 09:04:47 +0100

    server: prepare stallwart

--- systems/server/services/stalwart.nix
+++ systems/server/services/stalwart.nix
@@ -1,6 +1,5 @@
 utils: { config, services-root, domain-root, ... }:
 let
-  # ZFS
   bind-root = "${services-root}/stalwart";
 
   names = utils.createContainerNames "stalwart" [ "server" ];
@@ -9,6 +8,7 @@ let
     smtp = "stallwart-smtp";
     smtps = "stallwart-smtps";
     imaps = "stallwart-imaps";
+    jmaps = "stallwart-jmaps";
     https = "stallwart-https";
   };
 
@@ -22,6 +22,8 @@ in
       image = "stalwartlabs/mail-server:latest";
       volumes = [
         "${bind-root}:/opt/stalwart-mail"
+        "/services/certs:/data/certs:ro"
+        "/etc/localtime:/etc/localtime:ro"
       ];
 
       labels = {
@@ -32,29 +34,37 @@ in
         "traefik.tcp.routers.${services.smtp}.entrypoints" = "smtp";
         "traefik.tcp.routers.${services.smtp}.service" = "${services.smtp}";
         "traefik.tcp.services.${services.smtp}.loadbalancer.server.port" = "25";
+        "traefik.tcp.services.${services.smtp}.loadbalancer.proxyProtocol.version" = "2";
 
         # smtps
         "traefik.tcp.routers.${services.smtps}.rule" = "HostSNI(`${domain}`)";
         "traefik.tcp.routers.${services.smtps}.entrypoints" = "smtps";
         "traefik.tcp.routers.${services.smtps}.service" = "${services.smtps}";
-        "traefik.tcp.services.${services.smtps}.loadbalancer.server.port" = "25";
-        "traefik.tcp.routers.${services.smtps}.tls" = "true";
-        "traefik.tcp.routers.${services.smtps}.tls.certresolver" = "main";
+        "traefik.tcp.services.${services.smtps}.loadbalancer.server.port" = "465";
+        "traefik.tcp.services.${services.smtps}.loadbalancer.proxyProtocol.version" = "2";
+        "traefik.tcp.routers.${services.smtps}.tls.passthrough" = "true";
 
         # imaps
         "traefik.tcp.routers.${services.imaps}.rule" = "HostSNI(`${domain}`)";
         "traefik.tcp.routers.${services.imaps}.entrypoints" = "imaps";
         "traefik.tcp.routers.${services.imaps}.service" = "${services.imaps}";
-        "traefik.tcp.services.${services.imaps}.loadbalancer.server.port" = "143";
-        "traefik.tcp.routers.${services.imaps}.tls" = "true";
-        "traefik.tcp.routers.${services.imaps}.tls.certresolver" = "main";
+        "traefik.tcp.services.${services.imaps}.loadbalancer.server.port" = "993";
+        "traefik.tcp.services.${services.imaps}.loadbalancer.proxyProtocol.version" = "2";
+        "traefik.tcp.routers.${services.imaps}.tls.passthrough" = "true";
+
+        # jmap
+        "traefik.tcp.routers.${services.jmaps}.rule" = "HostSNI(`${domain}`)";
+        "traefik.tcp.routers.${services.jmaps}.entrypoints" = "https";
+        "traefik.tcp.routers.${services.jmaps}.service" = "${services.jmaps}";
+        "traefik.tcp.services.${services.jmaps}.loadbalancer.server.port" = "443";
+        "traefik.tcp.services.${services.jmaps}.loadbalancer.proxyProtocol.version" = "2";
+        "traefik.tcp.routers.${services.jmaps}.tls.passthrough" = "true";
+
 
         # https
-        "traefik.http.routers.${services.https}.rule" = "Host(`${domain}`)";
+        "traefik.http.routers.${services.https}.rule" = "Host(`${domain}`) || Host(`autodiscover.${domain-root}`) || Host(`autoconfig.${domain-root}`) || Host(`mta-sts.${domain-root}`)";
         "traefik.http.routers.${services.https}.service" = "${services.https}";
         "traefik.http.services.${services.https}.loadbalancer.server.port" = "8080";
-        "traefik.http.routers.${services.https}.tls" = "true";
-        "traefik.http.routers.${services.https}.tls.certresolver" = "main";
       };
     };
   };
--- systems/server/services/traefik.nix
+++ systems/server/services/traefik.nix
@@ -3,73 +3,95 @@ let
   username = config.users.users.main.name;
 
   acme-path = "${services-root}/acme.json";
+  certs-path = "${services-root}/certs";
 
   domain = "traefik.${domain-root}";
 in
 {
   systemd = {
-    tmpfiles.settings.acme_file."${acme-path}".f = {
-      user = username;
-      mode = "0600";
+    tmpfiles.settings = {
+      acme_file."${acme-path}".f = {
+        user = username;
+        mode = "0600";
+      };
+      certs-path."${certs-path}".d = {
+        user = username;
+        mode = "0600";
+      };
     };
   };
 
-  virtualisation.oci-containers.containers.traefik = {
-    image = "traefik:latest";
-    cmd = [
-      "--api=true"
+  virtualisation.oci-containers.containers = {
+    traefik = {
+      image = "traefik:latest";
+      cmd = [
+        "--api=true"
 
-      "--providers.docker=true"
-      "--providers.docker.exposedbydefault=false"
+        "--providers.docker=true"
+        "--providers.docker.exposedbydefault=false"
 
-      "--entryPoints.http.address=:80"
-      "--entryPoints.https.address=:443"
-      "--entryPoints.https.asDefault=true"
+        "--entryPoints.http.address=:80"
+        "--entryPoints.https.address=:443"
+        "--entryPoints.https.asDefault=true"
 
-      # "--entryPoints.DoT.address=:853"
+        # "--entryPoints.DoT.address=:853"
 
-      # mail
-      # "--entryPoints.smtp.address=:25"
-      # "--entryPoints.smtps.address=:465"
-      # "--entryPoints.imaps.address=:993"
+        # == mail
+        # smtp
+        # "--entryPoints.smtp.address=:25"
+        # smtps
+        # "--entryPoints.smtps.address=:465"
+        # imaps
+        # "--entryPoints.imaps.address=:993"
 
-      "--certificatesresolvers.main.acme.email=tornax@tornaxo7.de"
-      "--certificatesresolvers.main.acme.storage=acme.json"
-      "--certificatesresolvers.main.acme.httpchallenge.entrypoint=http"
-    ];
+        "--certificatesresolvers.main.acme.email=tornax@tornaxo7.de"
+        "--certificatesresolvers.main.acme.storage=acme.json"
+        "--certificatesresolvers.main.acme.httpchallenge.entrypoint=http"
+      ];
 
-    extraOptions = [
-      "--hostuser=${username}"
-    ];
+      extraOptions = [
+        "--hostuser=${username}"
+      ];
 
-    ports = [
-      "80:80"
-      "443:443"
+      ports = [
+        "80:80"
+        "443:443"
 
-      # DoT
-      # "853:853/tcp"
+        # DoT
+        # "853:853/tcp"
 
-      # mail
-      # "25:25/tcp"
-      # "465:465/tcp"
-      # "993:993/tcp"
-    ];
+        # mail
+        "25:25/tcp"
+        "465:465/tcp"
+        "993:993/tcp"
+      ];
 
-    volumes = [
-      "/var/run/podman/podman.sock:/var/run/docker.sock"
-      "${acme-path}:/acme.json"
-      "/etc/passwd:/etc/passwd:ro"
-    ];
+      volumes = [
+        "/var/run/podman/podman.sock:/var/run/docker.sock"
+        "${acme-path}:/acme.json"
+        "/etc/passwd:/etc/passwd:ro"
+      ];
 
-    labels = {
-      "traefik.enable" = "true";
-      "traefik.http.routers.dashboard.rule" = "Host(`${domain}`)";
-      "traefik.http.routers.dashboard.service" = "api@internal";
-      "traefik.http.routers.dashboard.tls" = "true";
-      "traefik.http.routers.dashboard.tls.certresolver" = "main";
+      labels = {
+        "traefik.enable" = "true";
+        "traefik.http.routers.dashboard.rule" = "Host(`${domain}`)";
+        "traefik.http.routers.dashboard.service" = "api@internal";
+        "traefik.http.routers.dashboard.tls" = "true";
+        "traefik.http.routers.dashboard.tls.certresolver" = "main";
 
-      "traefik.http.routers.dashboard.middlewares" = "auth";
-      "traefik.http.middlewares.auth.digestauth.users" = "tornax:traefik:6080745fca78301e72297e62cf416a3b";
+        "traefik.http.routers.dashboard.middlewares" = "auth";
+        "traefik.http.middlewares.auth.digestauth.users" = "tornax:traefik:6080745fca78301e72297e62cf416a3b";
+      };
     };
+
+    # traefik-certs-dumper = {
+    #   image = "ghcr.io/kereis/traefik-certs-dumper:latest";
+    #   dependsOn = [ "traefik" ];
+    #   volumes = [
+    #     "/etc/localtime:/etc/localtime:ro"
+    #     "${acme-path}:/traefik/acme.json:ro"
+    #     "${certs-path}:/output:rw"
+    #   ];
+    # };
   };
 }
