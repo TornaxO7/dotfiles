9e9dfc6c973c188b82984db18cdd21666c8f8bc9
TornaxO7 <tornax@proton.me>
2025-01-05 08:04:09 +0100

    reorder locales

--- modules/default.nix
+++ modules/default.nix
@@ -63,9 +63,9 @@ hostname:
         LC_MEASUREMENT = "de_DE.UTF-8";
         LC_MONETARY = "de_DE.UTF-8";
         LC_NAME = "de_DE.UTF-8";
-        LC_NUMERIC = "de_DE.UTF-8";
         LC_PAPER = "de_DE.UTF-8";
         LC_TELEPHONE = "de_DE.UTF-8";
+        LC_NUMERIC = "de_DE.UTF-8";
         LC_TIME = "de_DE.UTF-8";
       };
     };
