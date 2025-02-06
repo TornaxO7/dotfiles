d2f1c9a298740d4e575dce76760cbfeecd5fdd51
TornaxO7 <tornax@proton.me>
2025-02-06 14:47:59 +0100

    update rio

--- home/desktop/default/programs/rio/default.nix
+++ home/desktop/default/programs/rio/default.nix
@@ -1,10 +1,10 @@
-{ config, unstable, ... }:
+{ config, inputs, pkgs, ... }:
 {
   config = {
     programs.rio = {
       enable = true;
-      # package = inputs.rio.packages.${pkgs.system}.default;
-      package = unstable.rio;
+      package = inputs.rio.packages.${pkgs.system}.default;
+      # package = unstable.rio;
     };
 
     xdg.configFile.rio = {
