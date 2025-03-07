786d5827123d27b6337a412885185f2c5ed3e8bc
TornaxO7 <tornax@proton.me>
2025-03-07 12:21:26 +0100

    rekey secrets

--- modules/home-manager/packages.nix
+++ modules/home-manager/packages.nix
@@ -14,7 +14,7 @@ let
     ouch
     ripgrep
     ripgrep-all
-    rustic-rs
+    rustic
     sd
     spotify-player
     television
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,7 @@
 let
   pc = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEwQ1FO2lkd7ecYc/3GCo2yTWgo1V86uYUpX87bzFPhU tornax@pc";
   laptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB+jyIDy4aQ8KuJ2sJk3lnMYkE/8wDI7Vy9anrvRIKDL tornax@laptop";
-  nas = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA8MZMcbVs9Wr6f1VMUcYq1Aftbz8e/hekpto4yhwPIO tornax@nas";
+  nas = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPL8dets3tSpj6vvCqDgpudVWnDkYos2SRZEiys3eqSK tornax@nas";
   server = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMsm8adwS6SbUwBDhOTqpmzbC+s9oqmtkNAP7DO/txos root@server";
 
   main = [ pc laptop nas ];
