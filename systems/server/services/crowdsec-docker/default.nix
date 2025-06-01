cd2e6b2829f07468ccc04793dce73c3fa77f998a
TornaxO7 <tornax@proton.me>
2025-06-01 10:19:51 +0200

    server: focussing on just the crowdsec server

--- systems/server/services/crowdsec-docker/default.nix
+++ systems/server/services/crowdsec-docker/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ ... }:
 let
   utils = import ../../../utils.nix;
 
@@ -10,12 +10,12 @@ let
     acquis = "${conf}/acquis.d";
   };
 
-  network = "crowdsec";
+  ports = {
+    server = 49180;
+  };
 in
 {
   systemd = {
-    services.create-crowdsec-network = utils.createPodmanNetworkService pkgs network (builtins.attrValues names.service-full);
-
     tmpfiles.rules = [
       "d ${binds.data} 0750 - - -"
       "d ${binds.conf} 0750 - - -"
@@ -43,22 +43,23 @@ in
         ";
       };
 
-      networks = [ network ];
-    };
-
-    "${names.containers.firewall-bouncer}" = {
-      image = "ghcr.io/shgew/cs-firewall-bouncer-docker:latest";
-      extraOptions = [ "--network=host" ];
-      capabilities = {
-        NET_ADMIN = true;
-        NET_RAW = true;
-      };
-
-      volumes = [
-        "/etc/localtime:/etc/localtime:ro"
+      ports = [
+        "127.0.0.1:${toString ports.server}:8080"
       ];
-
-      networks = [ network ];
     };
+
+    # "${names.containers.firewall-bouncer}" = {
+    #   image = "ghcr.io/shgew/cs-firewall-bouncer-docker:latest";
+    #   extraOptions = [ "--network=host" ];
+    #   capabilities = {
+    #     NET_ADMIN = true;
+    #     NET_RAW = true;
+    #   };
+
+    #   volumes = [
+    #     "${./crowdsec-firewall-bouncer.yaml}:/config/crowdsec-firewall-bouncer.yaml:ro"
+    #     "/etc/localtime:/etc/localtime:ro"
+    #   ];
+    # };
   };
 }
