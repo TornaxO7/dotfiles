0a165a0b3c94a97c70b6c08336d499b0424e2e9f
TornaxO7 <tornax@proton.me>
2025-09-29 11:13:15 +0200

    nas: update filebrowser config
    RIP: https://github.com/filebrowser/filebrowser/issues/5363

--- systems/nas/services/filebrowser.nix
+++ systems/nas/services/filebrowser.nix
@@ -1,35 +1,24 @@
-utils: { config, zpool-root, domain-root, services-root, ... }:
+utils: { config, zpool-root, domain-root, ... }:
 let
   prefix = "filebrowser";
-
-  paths = rec {
-    root = "${services-root}/${prefix}";
-    db = "${root}/database.db";
-  };
 
   domain = "${prefix}.${domain-root}";
 in
 {
   config = {
-    systemd.tmpfiles = {
-      rules = [
-        "d ${paths.root} 0744 - - - -"
-        "f+ ${paths.db} 0644 tornax - - -"
-      ];
-    };
-
     virtualisation.oci-containers.containers.filebrowser = {
       image = "filebrowser/filebrowser";
       volumes = [
         "${zpool-root}/syncthing:/srv"
-        "${paths.db}:/database.db"
+        "filebrowser-db:/database"
+        "filebrowser-conf:/config"
       ];
 
       labels = {
         "traefik.enable" = "true";
         "traefik.http.routers.filebrowser.rule" = "Host(`${domain}`)";
         "traefik.http.routers.filebrowser.service" = "filebrowser";
-        "traefik.http.services.filebrowser.loadbalancer.server.port" = "80";
+        "traefik.http.services.filebrowser.loadbalancer.server.port" = "8080";
       };
     };
   };
