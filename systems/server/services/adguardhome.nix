228bebd2ce312215dd9f0ab0cffdb2de3fff2b9c
TornaxO7 <tornax@proton.me>
2025-12-31 18:25:15 +0100

    server: update dns adresses

--- systems/server/services/adguardhome.nix
+++ systems/server/services/adguardhome.nix
@@ -56,43 +56,63 @@ in
         }
       ];
 
-      dns = {
+      dns = rec {
         bind_hosts = [ ip-addr ];
         port = 53;
         ratelimit = 0;
         anonymize_client_ip = false;
+        edns_client_subnet.enabled = true;
         upstream_dns = [
           # -- adguard
-          "tls://94.140.14.14"
-          "tls://94.140.15.15"
+          "https://dns.adguard-dns.com/dns-query"
+          "tls://dns.adguard-dns.com"
+          "quic://dns.adguard-dns.com"
+          "sdns://AQIAAAAAAAAAETk0LjE0MC4xNC4xNDo1NDQzINErR_JS3PLCu_iZEIbq95zkSV2LFsigxDIuUso_OQhzIjIuZG5zY3J5cHQuZGVmYXVsdC5uczEuYWRndWFyZC5jb20"
+          "sdns://AQIAAAAAAAAAGFsyYTEwOjUwYzA6OmFkMTpmZl06NTQ0MyDRK0fyUtzywrv4mRCG6vec5EldixbIoMQyLlLKPzkIcyIyLmRuc2NyeXB0LmRlZmF1bHQubnMxLmFkZ3VhcmQuY29t"
 
           # -- cloudflare
-          "tls://1.1.1.1"
-          "tls://1.0.0.1"
+          "tls://one.one.one.one"
 
-          # -- mullvad
-          "tls://base.dns.mullvad.net"
-          "https://base.dns.mullvad.net/dns-query"
+          # -- google
+          "https://dns.google/dns-query"
+          "tls://dns.google"
+
+          # -- quad
+          "sdns://AQMAAAAAAAAADDkuOS45Ljk6ODQ0MyBnyEe4yHWM0SAkVUO-dWdG3zTfHYTAC4xHA2jfgh2GPhkyLmRuc2NyeXB0LWNlcnQucXVhZDkubmV0"
+          "sdns://AQMAAAAAAAAAElsyNjIwOmZlOjpmZV06ODQ0MyBnyEe4yHWM0SAkVUO-dWdG3zTfHYTAC4xHA2jfgh2GPhkyLmRuc2NyeXB0LWNlcnQucXVhZDkubmV0"
+          "https://dns.quad9.net/dns-query"
+          "tls://dns.quad9.net"
         ];
 
         bootstrap_dns = [
           # -- adguard
-          "tls://94.140.14.14"
-          "tls://94.140.15.15"
-
-          # -- cloudflare
-          "tls://1.1.1.1"
-        ];
-
-        fallback_dns = [
-          "1.1.1.1"
-
-          # adguard
           "94.140.14.14"
           "94.140.15.15"
+          "2a10:50c0::ad1:ff"
+          "2a10:50c0::ad2:ff"
+
+          # -- cloudflare
+          "1.1.1.1"
+          "1.0.0.1"
+          "2606:4700:4700::1111"
+          "2606:4700:4700::1001"
+
+          # -- google
+          "8.8.8.8"
+          "8.8.4.4"
+          "2001:4860:4860::8888"
+          "2001:4860:4860::8844"
+
+          # -- quad
+          "9.9.9.9"
+          "149.112.112.112"
+          "2620:fe::fe"
+          "2620:fe::9"
         ];
 
-        upstream_mode = "fastest_addr";
+        fallback_dns = bootstrap_dns;
+
+        upstream_mode = "parallel";
       };
 
       clients.persistent =
