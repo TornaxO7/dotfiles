8c8d9c3cb9b65131b34f51e2ad160ebe96a29424
TornaxO7 <tornax@proton.me>
2026-01-14 14:16:23 +0100

    server(anubis): fix bot policy

--- systems/server/services/anubis.nix
+++ systems/server/services/anubis.nix
@@ -10,6 +10,99 @@ in
         settings = {
           WEBMASTER_EMAIL = "tornax@pm.me";
         };
+
+        botPolicy = {
+          bots = [
+            { import = "(data)/meta/default-config.yaml"; }
+            {
+              name = "low-load-average";
+              action = "WEIGH";
+              expression = "load_15m <= 4.0";
+              weight.adjust = -10;
+            }
+          ];
+
+          dnsbl = false;
+
+          impressum = {
+            footer = ''
+              This website is hosted by TornaxO7. If you have any complaints or notes 
+              about the service,
+              please contact <a href="mailto:tornax@pm.me">tornax@pm.me</a> and we will assist you as soon as possible.
+            '';
+            page = {
+              title = "Imprint and Privacy Policy";
+              body = ''
+                <h2>Information that is gathered from visitors</h2>
+                <p>In common with other websites, log files are stored on the web server saving details such as the visitor's IP address, browser type, referring page and time of visit.</p>
+                <p>Cookies may be used to remember visitor preferences when interacting with the website.</p>
+              '';
+            };
+          };
+
+          openGraph.enabled = false;
+
+          status_codes = {
+            CHALLENGE = 403;
+            DENY = 403;
+          };
+
+          store.backend = "memory";
+
+          thresholds = [
+            {
+              name = "minimal-suspicion";
+              expression = "weight <= 0";
+              action = "ALLOW";
+            }
+            {
+              name = "mild-suspicion";
+              expression.all = [
+                "weight > 0"
+                "weight < 10"
+              ];
+              action = "CHALLENGE";
+              challenge = {
+                algorithm = "metarefresh";
+                difficulty = 1;
+              };
+            }
+            {
+              name = "moderate-suspicion";
+              expression.all = [
+                "weight >= 10"
+                "weight < 20"
+              ];
+              action = "CHALLENGE";
+              challenge = {
+                # https://anubis.techaro.lol/docs/admin/configuration/challenges/proof-of-work
+                algorithm = "fast";
+                difficulty = 2;
+              };
+            }
+            {
+              name = "mild-proof-of-work";
+              expression.all = [
+                "weight >= 20"
+                "weight < 30"
+              ];
+              action = "CHALLENGE";
+              challenge = {
+                algorithm = "fast";
+                difficulty = 4;
+              };
+            }
+            {
+              name = "extreme-suspicion";
+              expression = "weight >= 30";
+              action = "CHALLENGE";
+              challenge = {
+                algorithm = "fast";
+                difficulty = 6;
+              };
+            }
+          ];
+        };
       };
 
       instances = {
