349ec17fdcec05edb500148367304e6be7bd3686
TornaxO7 <tornax@proton.me>
2026-01-21 07:09:09 +0100

    yazi: remove substituters

--- modules/home-manager/programs/yazi/default.nix
+++ modules/home-manager/programs/yazi/default.nix
@@ -1,20 +1,7 @@
-{ config, unstable, pkgs, ... }:
+{ unstable, pkgs, ... }:
 {
   config = {
-    nix.settings = {
-      extra-substituters = [ "https://yazi.cachix.org" ];
-      extra-trusted-public-keys = [ "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k=" ];
-    };
-
     programs = {
-      # fish.functions.y = ''
-      #     set tmp (mktemp -t "yazi-cwd.XXXXXX")
-      #   	yazi $argv --cwd-file="$tmp"
-      #   	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-      #   		builtin cd -- "$cwd"
-      #   	end
-      #   	rm -f -- "$tmp"
-      # '';
       yazi = {
         enable = true;
         package = unstable.yazi;
