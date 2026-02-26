5b92189acfc54b782d16923dbbca0dc4925456f7
TornaxO7 <tornax@proton.me>
2026-02-26 13:01:58 +0100

    remove deprecated `pkgs.system`

--- modules/home-manager/programs/helix.nix
+++ modules/home-manager/programs/helix.nix
@@ -34,7 +34,7 @@ in
 
   programs.helix = {
     enable = true;
-    package = inputs.helix.packages.${pkgs.system}.default;
+    package = inputs.helix.packages.${pkgs.stdenv.hostPlatform.system}.default;
     defaultEditor = true;
 
     languages = {
