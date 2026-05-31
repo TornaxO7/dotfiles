c11475c554d0805180ead215b01f8230baa37e54
TornaxO7 <tornax@proton.me>
2026-05-31 12:40:36 +0200

    yazi: add chachix

--- modules/home-manager/programs/yazi/default.nix
+++ modules/home-manager/programs/yazi/default.nix
@@ -1,6 +1,11 @@
 { unstable, pkgs, ... }:
 {
   config = {
+    nix.settings = {
+      extra-substituters = [ "https://yazi.cachix.org" ];
+      extra-trusted-public-keys = [ "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k=" ];
+    };
+
     programs = {
       yazi = {
         enable = true;
