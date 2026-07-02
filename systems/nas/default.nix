613422d010b9b93ba1b9bdc01c9443230ec537be
TornaxO7 <tornax@proton.me>
2026-07-02 12:53:12 +0200

    nas: free vikunja from utils.nix

--- systems/nas/default.nix
+++ systems/nas/default.nix
@@ -32,7 +32,7 @@ in
     # each service here, can have a port, starting from 49200 (incrementing 10)
     ./services/jellyfin.nix
     ./services/filebrowser.nix
-    (loadService ./services/vikunja.nix)
+    ./services/vikunja.nix
     (loadService ./services/gotify.nix)
     ./services/timetagger.nix
   ];
