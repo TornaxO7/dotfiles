2493435e2a509eb81b354be226281b7f458d3fff
TornaxO7 <tornax@proton.me>
2026-09-13 16:02:49 +0200

    add ugreen led commmand

--- systems/nas/led.nix
+++ systems/nas/led.nix
@@ -1,5 +1,6 @@
 { pkgs, ... }:
 {
+  # sudo ugreen_leds_cli power -off
   services.udev.packages = with pkgs; [ rwedid ];
   environment.systemPackages = with pkgs; [ ugreen-leds-cli ];
   boot.kernelModules = [ "i2c-dev" ];
